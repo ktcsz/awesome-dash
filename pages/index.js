ec06e694c464021f86cc8c83a8df1cbe1ca6ac4c
ktc.shenzhen@gmail.com <ktcsz>
2022-09-20 17:39:37 +0000

    prisma init

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Hello</h1>
+        <h1 className={styles.title}>Hello World</h1>
       </main>
 
       <footer className={styles.footer}>
